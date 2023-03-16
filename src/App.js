b8c55029a886ab6c10b3ea4172e8a2d00dbbe269
Andre Hilario <andre18hilario@gmail.com>
2023-03-16 08:03:46 -0300

    AuthSupplier imported on App

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import HabitsPage from "./pages/HabitsPage/HabitsPage";
 import TodayPage from "./pages/TodayPage/TodayPage";
 import HistoricPage from "./pages/HistoricPage/HistoricPage";
+import { AuthSupplier } from "./context/AuthSupplier";
 
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />}></Route>
-        <Route path="/cadastro" element={<RegisterPage />}></Route>
-        <Route path="/habitos" element={<HabitsPage />}></Route>
-        <Route path="/hoje" element={<TodayPage />}></Route>
-        <Route path="/historico" element={<HistoricPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthSupplier>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/cadastro" element={<RegisterPage />}></Route>
+          <Route path="/habitos" element={<HabitsPage />}></Route>
+          <Route path="/hoje" element={<TodayPage />}></Route>
+          <Route path="/historico" element={<HistoricPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthSupplier>
   );
 }
 
